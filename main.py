8c422fb851e3d057a957a286441636711a47ae75
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-02-10 23:24:10 +0530

    PortStemmer for Stemming

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import re
 from nltk.corpus import stopwords
+import re
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -13,7 +14,38 @@ import nltk
 nltk.download('stopwords')
 
 
-#print(stopwords.words('english'))
+# print(stopwords.words('english'))
+
+# loading the dataset to a pandas DataFrame
+
+news_dataset = pd.read_csv('news.csv')
+
+print(news_dataset.shape)
+
+
+print(news_dataset.head())
+
+#checking for missing values
+print(news_dataset.isnull().sum())
+
+#seperating data and labels
+
+X = news_dataset.drop(columns = 'label')
+Y = df['label']
+
+
+
+Stemming 
+
+stem = PorterStemmer()
+
+
+def stemming(text):
+    stem_text = re.sub('[^a-zA-Z]', ' ', text)
+    stem_text = stem_text.lower()
+    stem_text = stem_text.split()
+    stem_text = [stem.stem(word) for word in stem_text if not word in stopwords.words('english')]
+
 
 
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(stopwords.words('english'))
